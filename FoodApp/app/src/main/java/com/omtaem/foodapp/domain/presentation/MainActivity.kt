c547cb012b66304251dadaca5e16e35ef1acef2a
OMER FARUK GUZEL <58605364+omerfarukgzl@users.noreply.github.com>
2022-12-24 10:34:28 +0300

    add menu

--- FoodApp/app/src/main/java/com/omtaem/foodapp/domain/presentation/MainActivity.kt
+++ FoodApp/app/src/main/java/com/omtaem/foodapp/domain/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.omtaem.foodapp
+package com.omtaem.foodapp.domain.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.omtaem.foodapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
