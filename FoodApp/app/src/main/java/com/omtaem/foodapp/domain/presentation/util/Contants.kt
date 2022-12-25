f7d5d4360fae2bffa031a8dc2d8dddcb87b645ee
Muhammet Talha Ergün <mtalhaergun24@gmail.com>
2022-12-25 20:37:28 +0300

    Contants

---
+++ FoodApp/app/src/main/java/com/omtaem/foodapp/domain/presentation/util/Contants.kt
@@ -0,0 +1,27 @@
+package com.omtaem.foodapp.domain.presentation.util
+
+object Contants {
+    const val BASE_URL = "https://api.spoonacular.com"
+    const val API_KEY = "7b94ae7fd54d404992c7c3269d8d63d1"
+    const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
+
+
+    const val QUERY_SEARCH = "query"
+    const val QUERY_NUMBER = "number"
+    const val QUERY_API_KEY = "apiKey"
+    const val QUERY_TYPE = "type"
+    const val QUERY_DIET = "diet"
+    const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+    const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+    const val DEFAULT_RECIPES_NUMBER = "50"
+    const val DEFAULT_MEAL_TYPE = "main course"
+    const val DEFAULT_DIET_TYPE = "gluten free"
+
+
+    const val DATABASE_NAME = "recipes_database"
+    const val RECIPES_TABLE = "recipes_table"
+
+
+    const val FAVORITE_RECIPES_TABLE = "favorites_recipes_table"
+}
