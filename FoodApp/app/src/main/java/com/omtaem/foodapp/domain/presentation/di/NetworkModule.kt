9d276d25984255c60b0f46e3fb24a9f0c36181a9
Muhammet Talha Ergün <mtalhaergun24@gmail.com>
2022-12-26 21:00:13 +0300

    Singletons

---
+++ FoodApp/app/src/main/java/com/omtaem/foodapp/domain/presentation/di/NetworkModule.kt
@@ -0,0 +1,71 @@
+package com.omtaem.foodapp.domain.presentation.di
+
+import com.omtaem.foodapp.BuildConfig
+import com.omtaem.foodapp.domain.presentation.data.network.FoodApi
+import com.omtaem.foodapp.domain.presentation.util.Contants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+import kotlin.time.DurationUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggerInterceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+        return httpLoggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(20, DurationUnit.SECONDS)
+            .connectTimeout(20, DurationUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    //TODO: change the gsonConverter to moshi
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFoodRecipesApi(
+        retrofit: Retrofit
+    ): FoodApi {
+        return retrofit.create(FoodApi::class.java)
+    }
+}
